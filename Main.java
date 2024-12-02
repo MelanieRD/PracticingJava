db42396ea55a2b5f143b9aebb0106b4255b89daf
Melanie <melaniehernandez153@hotmail.com>
2024-12-02 06:59:05 -0400

    Practicing Java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+
+            int num1 = 10;
+            int num2 = 20;
+            char letter = 65;
+
+            double converter = num1 + 3.77;
+
+            int converterToInt = (int) converter;
+
+        System.out.println(num1 + num2 +" Y la letra es: "+ letter);
+        System.out.println(converter);
+        System.out.println(converterToInt);
+       
+    }
+}
+
+
+//Javac Main.java compila nuestro codigo
