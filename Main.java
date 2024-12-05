ddc45a3348fad10e12663c1c1cbf8fff1dc66803
Melanie <melaniehernandez153@hotmail.com>
2024-12-05 07:34:08 -0400

    Practicing Java 12/5 Classes

--- Main.java
+++ Main.java
@@ -1,27 +1,48 @@
 public class Main{
     public static void main(String[] args){
 
-          // Metodo que sumen dos numeros
-        sumMethod(10, 30);
+    Sword sword1 = new Sword();
+    sword1.name = "The DragonFlame";
+    sword1.damage = 59;
 
-        //Metodos para iterar una cadena con forEach;
-        String nombre = "MelanieHernandez";
-        //forEachStringLetter(nombre);
-        //System.out.println(nombre.toCharArray());
-
-        // Otra forma con forEach (expresion lambda)
-        nombre.chars().forEach(c -> System.out.println((char) c));
+    System.out.println("La espada seleccionada es: "+ sword1.name + " con un daño de "+ sword1.damage);
 
 
-       
-    }
 
-    static void sumMethod(int num1, int num2){
-        System.out.println( num1+num2);
-    }
+    //       // Metodo que sumen dos numeros
+    //     //sumMethod(10, 30);
 
-    static void forEachStringLetter(String string){
-        for(char letter : string.toCharArray()){ System.out.println(letter);} ;
+    //     //Metodos para iterar una cadena con forEach;
+    //     String nombre = "MelanieHernandez";
+    //     //forEachStringLetter(nombre);
+    //     //System.out.println(nombre.toCharArray());
+
+    //     // Otra forma con forEach (expresion lambda)
+    //     // nombre.chars().forEach(c -> System.out.println((char) c));
+
+    //         for(int i = 0 ; i < nombre.length(); i++ ){
+    //             System.out.println(nombre.charAt(i));
+    //         }
+    //     // Arrays Iteracion
+
+    //     String[] nombres = {"Melanie", "Delanie", "Virginia", "Daniel", "Maniga", "Luis"};
+
+    //       //nombres.forEach(nombr -> System.out.println(nombr));
+
+    //     for(String i : nombres){
+    //         System.out.println(i);
+    //     }
+
+
+
+    // }
+
+    // static void sumMethod(int num1, int num2){
+    //     System.out.println( num1+num2);
+    // }
+
+    // static void forEachStringLetter(String string){
+    //     for(char letter : string.toCharArray()){ System.out.println(letter);} ;
     }
 }
 
---
+++ Sword.java
@@ -0,0 +1,9 @@
+public class Sword {
+        String name = "Scalibur";
+        int damage = 35;
+        double criticRate = 0.10d;
+        double criticDamage = 0.35d;
+
+
+        
+}
