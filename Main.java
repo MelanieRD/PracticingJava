4e9d42cca35e4c2d0a23740767c32c5278293683
Melanie <melaniehernandez153@hotmail.com>
2024-12-05 06:12:01 -0400

    Practicing Java 12/5

--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
 public class Main{
     public static void main(String[] args){
 
-            int num1 = 10;
-            int num2 = 20;
-            char letter = 65;
+          // Metodo que sumen dos numeros
+        sumMethod(10, 30);
 
-            double converter = num1 + 3.77;
+        //Metodos para iterar una cadena con forEach;
+        String nombre = "MelanieHernandez";
+        //forEachStringLetter(nombre);
+        //System.out.println(nombre.toCharArray());
 
-            int converterToInt = (int) converter;
+        // Otra forma con forEach (expresion lambda)
+        nombre.chars().forEach(c -> System.out.println((char) c));
 
-        System.out.println(num1 + num2 +" Y la letra es: "+ letter);
-        System.out.println(converter);
-        System.out.println(converterToInt);
+
        
+    }
+
+    static void sumMethod(int num1, int num2){
+        System.out.println( num1+num2);
+    }
+
+    static void forEachStringLetter(String string){
+        for(char letter : string.toCharArray()){ System.out.println(letter);} ;
     }
 }
 
-
+// Scanner Class found in java.util package
 //Javac Main.java compila nuestro codigo
